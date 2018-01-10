2517570215a15e6205a21e64ec363c9d2de3846b
Tenyson <rojahog@hotmail.com>
2018-01-09 21:12:10 -0800

    ..

--- pyRockPaperScissors.py
+++ pyRockPaperScissors.py
@@ -1,20 +1,40 @@
 import random
 
 def play():
-  """
-  This function simply returns the option the player selected.
-  """
+  if(selection == 1):
+    return "Rock"
+  elif(selection == 2):
+    return "Paper"
+  elif(selection == 3):
+    return "Scissors"
 
 def Computer():
-	"""
-  This function should simply return the computers SINGLE choice of rock, paper, or scissors IN LOWERCASE
-  """
+  comChoice = random.randint(0, 2)
+  if(comChoice == 0):
+    comPlay = "Rock"
+    return comPlay
+  elif(comChoice == 1):
+    comPlay = "Paper"
+    return comPlay
+  elif(comChoice == 2):
+    comPlay = "Scissors"
+    return comPlay     
+  """This function should simply return the computers SINGLE choice of rock, paper, or scissors IN LOWERCASE"""
 
 def Human():
-	"""
-  This function should display a menu, prompting the player to select either rock, paper, or scissors and return what the player 
-  selected IN LOWERCASE . If the player selects an invalid option, display the error message "Invalid Play"
-  """
+  global selection
+  print("1. Rock")
+  print("2. Paper")
+  print("3. Scissors")
+  selection = int(input("Choose the number that corresponds with the move you want to make" + " "))
+  if selection == 1:
+    print("Rock".lower())
+  elif selection == 2:
+    print ("Paper".lower())
+  elif selection == 3:
+    print("Scissors".lower())
+  else:
+    print ("Invalid play")
 
 def main():
 	print("Welcome To PyRock Paper Scissors Best of 3\n")
@@ -25,4 +45,68 @@ def main():
   """
 	
 if __name__ == '__main__':
-	main()
+        main()
+        name = input("Enter Name" + " ")
+        nameScore = 0
+        ComputerScore = 0
+
+        for x in range(0, 3):
+          Human()
+          
+          print(play()+ " " + "vs" + " " +Computer())
+
+          if(play() == Computer()):
+            print("Draw!")
+          elif(play() == "Rock"):
+            if(Computer() == "paper"):
+              print("The computer won")
+            ComputerScore =  ComputerScore + 1
+          elif(play() == "Rock"):
+            if(Computer == "Scissors"):
+              print(name, "won")
+              nameScore = nameScore + 1        
+          elif(play() == "Scissors"):
+            if(Computer() == "Paper"):
+              print(name, "won")
+              nameScore = nameScore + 1
+          elif(play() == "Scissors"):
+             if(Computer() == "Rock"):
+               print("The computer won")
+               ComputerScore = ComputerScore + 1
+          elif(play() == "Paper"):
+              if(Computer() == "Rock"):
+                print(name, "won")
+                nameScore = nameScore + 1
+          elif(play() == "Paper"):
+            if(Computer() == "Scissors"):
+              print("The computer won")
+              ComputerScore = ComputerScore + 1
+
+          print(nameScore)
+          print(ComputerScore)
+          if(nameScore > ComputerScore):
+            print(name, "has won with", nameScore)
+          else:
+            print("The computer has won with", ComputerScore)
+
+
+           
+
+            
+
+              
+                     
+
+
+
+
+
+
+
+
+
+
+
+
+               
+        
